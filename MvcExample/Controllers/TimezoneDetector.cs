161a83ebdc86c63a16b9b56011925fc7abda3561
zsu <azure.ss@gmail.com>
2017-01-14 14:28:24 -0500

    add missing files

---
+++ MvcExample/Controllers/TimezoneDetector.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TimezoneDetector;
+namespace MvcExample.Controllers
+{
+	public class TimezoneDetectorController : Controller
+	{
+		public ActionResult Index()
+		{
+			ViewBag.ClientTime = DateTime.UtcNow.ToClientTime();
+			return View();
+		}
+	}
+}
